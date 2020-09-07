dcf07d9517e37683b9122f2a4b2a50e925bcac89
Anna <zoora1234@hotmail.com>
2020-09-07 11:30:17 +0200

    Refaced startup

--- app/src/main/java/com/example/marvellisimo/CharacterDetailsActivity.kt
+++ app/src/main/java/com/example/marvellisimo/CharacterDetailsActivity.kt
@@ -3,9 +3,9 @@ package com.example.marvellisimo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class CharacterDetailActivity : AppCompatActivity() {
+class CharacterDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character_detail)
+        setContentView(R.layout.activity_character_details)
     }
 }
