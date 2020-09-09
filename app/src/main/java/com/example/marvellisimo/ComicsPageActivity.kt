609b6092dea8d49c34efd1bd3e9d3b1218342e96
Anna <zoora1234@hotmail.com>
2020-09-09 16:12:07 +0200

    recycleView adapter in making (anna och sofia)

--- app/src/main/java/com/example/marvellisimo/ComicsPageActivity.kt
+++ app/src/main/java/com/example/marvellisimo/ComicsPageActivity.kt
@@ -7,6 +7,8 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_comic_page.*
+import kotlinx.android.synthetic.main.activity_comic_page.view.*
+import kotlinx.android.synthetic.main.comic_recycle_row_layout.view.*
 
 class ComicsPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +25,11 @@ class ComicsPageActivity : AppCompatActivity() {
 }
 
 //adapterClass:
-class ComicItem(): Item<GroupieViewHolder>(){
+class ComicItem(val character: Character): Item<GroupieViewHolder>(){
 
     //get the name and pic from viewModel-object: will be called in our list for each user object:
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-
+        viewHolder.itemView.textView_charactername_row.text = character.name
         // viewHolder.itemView.textView_username_new_message.text= user.username
         //put image in: (use picasso lib, 3e party: https://github.com/square/picasso)
 
