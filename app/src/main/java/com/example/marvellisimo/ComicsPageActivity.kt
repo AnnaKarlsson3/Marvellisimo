9be2189e82960c0bf340c9ebcdba46c001986460
Anna <zoora1234@hotmail.com>
2020-09-10 00:02:52 +0200

    recycleView adapter in making - can print names to recycleview from viewmodel

--- app/src/main/java/com/example/marvellisimo/ComicsPageActivity.kt
+++ app/src/main/java/com/example/marvellisimo/ComicsPageActivity.kt
@@ -2,12 +2,12 @@ package com.example.marvellisimo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.squareup.picasso.Picasso
+import android.util.Log
+import androidx.activity.viewModels
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_comic_page.*
-import kotlinx.android.synthetic.main.activity_comic_page.view.*
 import kotlinx.android.synthetic.main.comic_recycle_row_layout.view.*
 
 class ComicsPageActivity : AppCompatActivity() {
@@ -16,21 +16,29 @@ class ComicsPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_comic_page)
 
         //3party adapter https://github.com/lisawray/groupie ..
+
         val adapter = GroupAdapter<GroupieViewHolder>()
+        val model: ViewModelCharacterPage by viewModels()
 
-        //adapter.add()
+        model.characterDataWrapper.observe(this, {
+
+            it.data.results.forEach { character -> adapter.add(ComicItem(character))
+                Log.i("tag", "inViewObserv:${it}") }
+        })
+
         recycle_view_comic.adapter = adapter
-
     }
 }
 
+
+
 //adapterClass:
-class ComicItem(val character: Character): Item<GroupieViewHolder>(){
+class ComicItem(val character: Character) : Item<GroupieViewHolder>() {
 
     //get the name and pic from viewModel-object: will be called in our list for each user object:
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.itemView.textView_charactername_row.text = character.name
-        // viewHolder.itemView.textView_username_new_message.text= user.username
+        Log.i("tag", "in comicItem:${character.name}")
         //put image in: (use picasso lib, 3e party: https://github.com/square/picasso)
 
     }
