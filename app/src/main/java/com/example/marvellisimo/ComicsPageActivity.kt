3b3fce2c6ab48ec34acefaf72590ea20c04bf59a
Anna <zoora1234@hotmail.com>
2020-09-10 10:07:27 +0200

    recycleView adapter in making - made 2 recycleviews for each class

--- app/src/main/java/com/example/marvellisimo/ComicsPageActivity.kt
+++ app/src/main/java/com/example/marvellisimo/ComicsPageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.marvellisimo
 
+import ComicItem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.activity_comic_page.*
+import kotlinx.android.synthetic.main.character_recycle_row_layout.*
 import kotlinx.android.synthetic.main.comic_recycle_row_layout.view.*
 
 class ComicsPageActivity : AppCompatActivity() {
@@ -15,15 +17,16 @@ class ComicsPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comic_page)
 
-        //3party adapter https://github.com/lisawray/groupie ..
+        PrintToRecycleView()
+    }
 
+    private fun PrintToRecycleView(){
+        //3party adapter https://github.com/lisawray/groupie ..
         val adapter = GroupAdapter<GroupieViewHolder>()
         val model: ViewModelCharacterPage by viewModels()
 
         model.characterDataWrapper.observe(this, {
-
-            it.data.results.forEach { character -> adapter.add(ComicItem(character))
-                Log.i("tag", "inViewObserv:${it}") }
+            it.data.results.forEach { comic -> adapter.add(ComicItem(comic)) }
         })
 
         recycle_view_comic.adapter = adapter
@@ -31,20 +34,3 @@ class ComicsPageActivity : AppCompatActivity() {
 }
 
 
-
-//adapterClass:
-class ComicItem(val character: Character) : Item<GroupieViewHolder>() {
-
-    //get the name and pic from viewModel-object: will be called in our list for each user object:
-    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-        viewHolder.itemView.textView_charactername_row.text = character.name
-        Log.i("tag", "in comicItem:${character.name}")
-        //put image in: (use picasso lib, 3e party: https://github.com/square/picasso)
-
-    }
-
-    //renders out the rows in view:
-    override fun getLayout(): Int {
-        return R.layout.comic_recycle_row_layout
-    }
-}
