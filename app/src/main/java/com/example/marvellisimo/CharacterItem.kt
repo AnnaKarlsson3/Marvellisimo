9d95fbe3e6e5c656c50e9963b88c6bece52856dc
Sofia <sofia.fredmans@gmail.com>
2020-09-10 14:15:30 +0200

    added images to comics and characters needs to fix ui by Anna and Sofia

--- app/src/main/java/com/example/marvellisimo/CharacterItem.kt
+++ app/src/main/java/com/example/marvellisimo/CharacterItem.kt
@@ -1,5 +1,7 @@
+
 import com.example.marvellisimo.Character
 import com.example.marvellisimo.R
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
 import kotlinx.android.synthetic.main.character_recycle_row_layout.view.*
@@ -7,14 +9,22 @@ import kotlinx.android.synthetic.main.character_recycle_row_layout.view.*
 //adapterClass:
 class CharacterItem(val character: Character) : Item<GroupieViewHolder>() {
 
+
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.itemView.textView_charactername_row.text = character.name
-        //put image in: (use picasso lib, 3e party: https://github.com/square/picasso)
+
+        val imgsize = "/portrait_small"
+        val img : String = renamePathHttps("${character.thumbnail.path}$imgsize.${character.thumbnail.extension}")
+
+        Picasso.get().load(img).fit().into(viewHolder.itemView.imageView_character_row)
 
     }
 
     //renders out the rows in view:
     override fun getLayout(): Int {
         return R.layout.character_recycle_row_layout
+    }
+    fun renamePathHttps(path: String): String {
+        return path.replace("http", "https")
     }
 }
--- app/src/main/java/com/example/marvellisimo/ComicItem.kt
+++ app/src/main/java/com/example/marvellisimo/ComicItem.kt
@@ -1,9 +1,9 @@
+
 import com.example.marvellisimo.Character
 import com.example.marvellisimo.R
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
-import kotlinx.android.synthetic.main.character_recycle_row_layout.*
-import kotlinx.android.synthetic.main.character_recycle_row_layout.view.*
 import kotlinx.android.synthetic.main.comic_recycle_row_layout.view.*
 
 //adapterClass:
@@ -13,11 +13,21 @@ class ComicItem(val character: Character) : Item<GroupieViewHolder>() {
         viewHolder.itemView.textView_comicname_row.text = character.name
         //put image in: (use picasso lib, 3e party: https://github.com/square/picasso)
 
+        val imgsize = "/portrait_small"
+        val img : String = renamePathHttps("${character.thumbnail.path}$imgsize.${character.thumbnail.extension}")
+
+        Picasso.get().load(img).fit().into(viewHolder.itemView.imageView_comic_row)
+
+
     }
 
     //renders out the rows in view:
     override fun getLayout(): Int {
         return R.layout.comic_recycle_row_layout
+    }
+
+    fun renamePathHttps(path: String): String {
+        return path.replace("http", "https")
     }
 }
 
