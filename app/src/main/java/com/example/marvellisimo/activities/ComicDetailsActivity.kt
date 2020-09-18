fce6bc7eacfeedaf7b47d02c322b9d0faa0784f3
alebal14 <alexandra.b.balogh@gmail.com>
2020-09-18 15:25:06 +0200

    favorite on comic

--- app/src/main/java/com/example/marvellisimo/activities/ComicDetailsActivity.kt
+++ app/src/main/java/com/example/marvellisimo/activities/ComicDetailsActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.marvellisimo.CharactersPageActivity
 import com.example.marvellisimo.ComicsPageActivity
 import com.example.marvellisimo.R
 import com.example.marvellisimo.entity.RealmCharacterEntity
 import com.squareup.picasso.Picasso
 import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_character_details.*
 import kotlinx.android.synthetic.main.activity_comic_details.*
 
 
@@ -28,23 +30,50 @@ class ComicDetailsActivity : AppCompatActivity() {
         val info = intent.getStringExtra(ComicsPageActivity.COMIC_INFO)
         val imageUrl = intent.getStringExtra(ComicsPageActivity.COMIC_IMAGE)
         val url = intent.getStringExtra(ComicsPageActivity.COMIC_URL)
-        val favorite = intent.getBooleanExtra(ComicsPageActivity.COMIC_FAVORITE , false)
+        //val favorite = intent.getBooleanExtra(ComicsPageActivity.COMIC_FAVORITE , false)
 
         supportActionBar?.title = text
         comic_name.text = text
         comic_info.text = info
 
+
+
         val comicFromDatabase = ComicDetailsActivity.realm.where(RealmCharacterEntity::class.java)
             .equalTo("id", id)
             .findFirst()
 
-
+        val favorite = comicFromDatabase!!.favorite
 
         if (favorite == true) {
                 image_Fav_Button_comic.setImageResource(R.drawable.ic_star_solid)
             } else {
                 image_Fav_Button_comic.setImageResource(R.drawable.ic_star_regular)
             }
+
+
+        image_Fav_Button_character.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                if (favorite == false) {
+                    realm.executeTransaction {
+                        comicFromDatabase!!.favorite = true
+                        CharacterDetailsActivity.realm.copyToRealmOrUpdate(comicFromDatabase)
+                    }
+
+                    finish()
+                    startActivity(intent)
+
+                } else {
+                    realm.executeTransaction {
+                        comicFromDatabase!!.favorite = false
+                        CharacterDetailsActivity.realm.copyToRealmOrUpdate(comicFromDatabase)
+                    }
+
+                    finish()
+                    startActivity(intent)
+                }
+            }
+        });
+
 
 
         Picasso.get().load(imageUrl?.replace("http","https")).fit().into(comic_image)
