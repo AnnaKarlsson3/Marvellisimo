2d900bd3a2600a2a1077af8c3ea3b66678978932
alebal14 <alexandra.b.balogh@gmail.com>
2020-09-21 11:42:33 +0200

    master fix

--- app/src/main/java/com/example/marvellisimo/activities/CharacterDetailsActivity.kt
+++ app/src/main/java/com/example/marvellisimo/activities/CharacterDetailsActivity.kt
@@ -3,33 +3,46 @@ package com.example.marvellisimo.activities
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.marvellisimo.CharactersPageActivity
 import com.example.marvellisimo.R
+import com.example.marvellisimo.entity.RealmCharacterEntity
 import com.squareup.picasso.Picasso
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_character_details.*
 
 
-
 class CharacterDetailsActivity : AppCompatActivity() {
+    companion object {
+        val realm = Realm.getDefaultInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_details)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-
-
+        val id = intent.getIntExtra(CharactersPageActivity.CHAR_ID, 1)
         val text = intent.getStringExtra(CharactersPageActivity.CHAR_NAME)
         val info = intent.getStringExtra(CharactersPageActivity.CHAR_INFO)
         val imageUrl = intent.getStringExtra(CharactersPageActivity.CHAR_IMAGE)
         val url = intent.getStringExtra(CharactersPageActivity.CHAR_URL)
-        val favorite = intent.getBooleanExtra(CharactersPageActivity.CHAR_FAVORITE , false)
+
+        val characterFromDatabase = realm.where(RealmCharacterEntity::class.java)
+            .equalTo("id", id)
+            .findFirst()
+
+
+        var favorite = characterFromDatabase!!.favorite
 
         supportActionBar?.title = text
-
         character_name.text = text
         character_info.text = info
 
-        Picasso.get().load(imageUrl?.replace("http","https")).fit().into(character_image)
+        Picasso.get().load(imageUrl?.replace("http", "https")).fit().into(character_image)
+
 
         if (favorite == true) {
             image_Fav_Button_character.setImageResource(R.drawable.ic_star_solid)
@@ -37,11 +50,31 @@ class CharacterDetailsActivity : AppCompatActivity() {
             image_Fav_Button_character.setImageResource(R.drawable.ic_star_regular)
         }
 
+        image_Fav_Button_character.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                if (characterFromDatabase.favorite == false) {
+                    realm.executeTransaction {
+                        characterFromDatabase!!.favorite = true
+                        realm.copyToRealmOrUpdate(characterFromDatabase)
+                    }
+                    image_Fav_Button_character.setImageResource(R.drawable.ic_star_solid)
+
+                } else {
+
+                    realm.executeTransaction {
+                        characterFromDatabase!!.favorite = false
+                        realm.copyToRealmOrUpdate(characterFromDatabase)
+                    }
+                    image_Fav_Button_character.setImageResource(R.drawable.ic_star_regular)
+
+                }
+            }
+        })
+
 
         character_link.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(url)
-
             startActivity(intent)
         }
 
@@ -49,3 +82,4 @@ class CharacterDetailsActivity : AppCompatActivity() {
     }
 
 }
+
--- app/src/main/java/com/example/marvellisimo/activities/ComicDetailsActivity.kt
+++ app/src/main/java/com/example/marvellisimo/activities/ComicDetailsActivity.kt
@@ -2,51 +2,85 @@ package com.example.marvellisimo.activities
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.marvellisimo.ComicsPageActivity
 import com.example.marvellisimo.R
+import com.example.marvellisimo.entity.RealmComicEntity
 import com.squareup.picasso.Picasso
+import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_comic_details.*
 
 
-
 class ComicDetailsActivity : AppCompatActivity() {
+    companion object {
+        val realm = Realm.getDefaultInstance()
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comic_details)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
+
+
+
+        val id = intent.getIntExtra(ComicsPageActivity.COMIC_ID, 1)
         val text = intent.getStringExtra(ComicsPageActivity.COMIC_TITLE)
         val info = intent.getStringExtra(ComicsPageActivity.COMIC_INFO)
         val imageUrl = intent.getStringExtra(ComicsPageActivity.COMIC_IMAGE)
         val url = intent.getStringExtra(ComicsPageActivity.COMIC_URL)
-        val favorite = intent.getBooleanExtra(ComicsPageActivity.COMIC_FAVORITE , false)
 
-        supportActionBar?.title = text
+
+
         comic_name.text = text
         comic_info.text = info
 
+        val comicFromDatabase = ComicDetailsActivity.realm.where(RealmComicEntity::class.java)
+            .equalTo("id", id)
+            .findFirst()
+
+        var favorite = comicFromDatabase!!.favorite
+
+        if (favorite == true) {
+            image_Fav_Button_comic.setImageResource(R.drawable.ic_star_solid)
+        } else {
+            image_Fav_Button_comic.setImageResource(R.drawable.ic_star_regular)
+        }
 
 
-            if (favorite == true) {
-                image_Fav_Button_comic.setImageResource(R.drawable.ic_star_solid)
-            } else {
-                image_Fav_Button_comic.setImageResource(R.drawable.ic_star_regular)
+
+        image_Fav_Button_comic.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                if (comicFromDatabase.favorite == false) {
+                    realm.executeTransaction {
+                        comicFromDatabase!!.favorite = true
+                        realm.copyToRealmOrUpdate(comicFromDatabase)
+                    }
+                    image_Fav_Button_comic.setImageResource(R.drawable.ic_star_solid)
+
+                } else {
+                    image_Fav_Button_comic.setImageResource(R.drawable.ic_star_regular)
+                    realm.executeTransaction {
+                        comicFromDatabase!!.favorite = false
+                        realm.copyToRealmOrUpdate(comicFromDatabase)
+                    }
+
+                }
             }
+        })
 
 
-        Picasso.get().load(imageUrl?.replace("http","https")).fit().into(comic_image)
+        Picasso.get().load(imageUrl?.replace("http", "https")).fit().into(comic_image)
 
         comic_link.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
-
             intent.data = Uri.parse(url)
-
             startActivity(intent)
         }
 
     }
 
 }
-
-
