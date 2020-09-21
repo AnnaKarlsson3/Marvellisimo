2724e08521075fb25dff66252112485f26392bac
alebal14 <alexandra.b.balogh@gmail.com>
2020-09-21 11:09:39 +0200

    master fix

--- app/src/main/java/com/example/marvellisimo/dao/RealmDao.kt
+++
@@ -1,7 +0,0 @@
-package com.example.marvellisimo.dao
-
-import io.realm.RealmModel
-import io.realm.RealmResults
-
-class RealmDao
-fun <T: RealmModel> RealmResults<T>.asLiveData() = RealmLivedata<T>(this)
