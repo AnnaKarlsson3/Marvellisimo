ad9fe9f79f396b39507bd14efb87657380afbd86
snehal patel <snehal.kathiriya@gmail.com>
2020-09-24 14:27:55 +0200

    .

--- app/src/main/java/com/example/marvellisimo/activities/LoginPageActivity.kt
+++ app/src/main/java/com/example/marvellisimo/activities/LoginPageActivity.kt
@@ -13,7 +13,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.coroutines.tasks.await
 
 
 class LoginPageActivity : AppCompatActivity() {
