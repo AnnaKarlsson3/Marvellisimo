7cabbdd85626a01a441d5c5e9237086be67d8438
alebal14 <alexandra.b.balogh@gmail.com>
2020-09-27 16:30:35 +0200

    Revert "update"
    This reverts commit 4beb3697a58d7a3c24329796a2897830f5c35716.

--- app/src/main/java/com/example/marvellisimo/activities/ComicDetailsActivity.kt
+++ app/src/main/java/com/example/marvellisimo/activities/ComicDetailsActivity.kt
@@ -1,7 +1,6 @@
 package com.example.marvellisimo.activities
 
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
@@ -70,6 +69,10 @@ class ComicDetailsActivity : AppCompatActivity() {
 
 
 
+        share_comic_detailview .setOnClickListener{
+            // Show the single choice list items on an alert dialog
+            showDialog()
+        }
 
 
         image_Fav_Button_comic.setOnClickListener(object : View.OnClickListener {
@@ -99,23 +102,232 @@ class ComicDetailsActivity : AppCompatActivity() {
         }
 
 
+        drawerListener()
+        displayCurrentUserInNav()
+        fetchUsersAndDisplayInNav()
     }
 
 
+
+    private fun drawerListener (){
+        drawerLayout_co_detail.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.exit_icon -> {
+                //set boolean active in db to false when logging out:
+                val ref = FirebaseDatabase.getInstance().getReference("/users")
+                val user = Firebase.auth.currentUser
+                val userid = user?.uid
+
+                if (userid != null) {
+                    ref.child(userid).child("active").setValue(false)
+                }
+
+                FirebaseAuth.getInstance().signOut()
+
+                //go back to login intent:
+                val intent = Intent(this, LoginPageActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+            }
+        }
+
+        return if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        else{
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_nav, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun displayCurrentUserInNav(){
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        val user = Firebase.auth.currentUser
+        val userid = user?.uid
+
+        if (userid != null) {
+            ref.child(userid).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val name = snapshot.child("username").value.toString()
+                    val image = snapshot.child("imageUrl").value.toString()
+                    inlogged_username_co_detail.text = name
+
+                    ComicsPageActivity.currentUser = snapshot.getValue(User::class.java)
+
+                    Picasso.get()
+                        .load(image)
+                        .resize(50, 50)
+                        .transform(CropCircleTransformation())
+                        .into(inlogged_userImg_co_detail)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+        }
+    }
+
+
+
+
+
+
+    private fun showDialog() {
+
+
+        // Initialize a new instance of alert dialog builder object
+        val builder = AlertDialog.Builder(this)
+
+        // Set a title for alert dialog
+        builder.setTitle("Choose an user")
+
+        val users = mutableListOf<UserItem>()
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+
+
+        ref.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val user = snapshot.getValue(User::class.java)
+                if (user != null) {
+                    val user = UserItem(user)
+                    users.add(user)
+
+                    Log.d("UserList" , "${users}")
+                    builder.adapter
+                }
+
+
+                for (u in users) {
+                    Log.d("usersCharacter", "users in list: ${u.user.username}")
+                }
+
+
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val user = snapshot.getValue(User::class.java)
+                if (user != null) {
+                    val oldUser =
+                        users.find { it.user.uid == user.uid } //hitta user i listan som har samma uid som den i db har som ändrats
+                    oldUser?.user?.active = user.active
+
+
+                }
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+
+
+                TODO()
+
+            }
+
+
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+
+
+        // Create a new AlertDialog using builder object
+        val dialog = builder.create()
+
+        // Finally, display the alert dialog
+        dialog.show()
+
+    }
+
+    //Displays all users
+    private fun fetchUsersAndDisplayInNav(){
+        val users = mutableListOf<UserItem>()
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        val adapterNav = GroupAdapter<GroupieViewHolder>()
+
+        ref.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val user = snapshot.getValue(User::class.java)
+                if (user != null) {
+                    val user = UserItem(user)
+                    adapterNav.add(user)
+                    users.add(user)
+                }
+                toolBar_RecyclerView_co_detail.adapter = adapterNav
+
+                for (u in users) {
+                    Log.d("usersCharacter", "users in list: ${u.user.username}")
+                }
+
+                adapterNav.setOnItemClickListener { item, view ->
+                    val userItem = item as UserItem
+                    val intent = Intent(view.context, SendMessageActivity::class.java)
+                    intent.putExtra(ComicsPageActivity.USER_KEY, userItem.user)
+                    intent.putExtra(ComicsPageActivity.USER_NAME, userItem.user.username)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                val user = snapshot.getValue(User::class.java)
+                if (user != null) {
+                    val oldUser =
+                        users.find { it.user.uid == user.uid } //hitta user i listan som har samma uid som den i db har som ändrats
+                    oldUser?.user?.active = user.active
+
+                    adapterNav.notifyDataSetChanged()
+                }
+                toolBar_RecyclerView_co_detail.adapter = adapterNav
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+
+
+                TODO()
+
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+    }
+
+
+
+override fun onDestroy() {
+        //set boolean active in db to false when logging out:
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        val user = Firebase.auth.currentUser
+        val userid = user?.uid
+
+        if (userid != null) {
+            ref.child(userid).child("active").setValue(false)
+        }
+        FirebaseAuth.getInstance().signOut()
+        super.onDestroy()
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
